8a3c5fd293ea3eacc9dfdeeda245c8288c9af3eb
Brunopuod <80692024+Brundex@users.noreply.github.com>
2024-03-18 23:10:14 -0300

    Add files via upload

---
+++ pre-entrega2/src/index.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { Server } from 'socket.io'
+import { engine } from 'express-handlebars'
+import { __dirname } from './path.js'
+
+//Configuraciones o declaraciones
+const app = express()
+const PORT = 8000
+
+//Server
+const server = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+
+const io = new Server(server)
+
+//Connection DB
+mongoose.connect("mongodb+srv://puodziunasb:9xV!wL9VnHeEqg@cluster0.s05wzm8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("DB is connected")).catch(e => console.log(e))
+
+//Middlewares
+app.use(express.json())
+app.use(session({
+    secret: "coderSecret",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(cookieParser("claveSecreta"))
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
+
+app.use('/', indexRouter)
+
+
+io.on('connection', (socket) => {
+    console.log("Conexion con Socket.io")
+
+    socket.on('mensaje', async (mensaje) => {
+        try {
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        } catch (e) {
+            io.emit('mensajeLogs', e)
+        }
+
+    })
+
+})
